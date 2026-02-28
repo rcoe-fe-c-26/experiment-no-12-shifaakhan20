f41019ee04cbbbf797ea72a237cddf2feab1bfe6
shifaakhan20 <shifaakhan860@gmail.com>
2026-03-01 01:46:01 +0530

    Enhance word extraction program with details and logic
    Updated coder name and date in comments. Added functionality to read words of specified lengths from a text file.

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 # AIM: Develop a Python program that reads a text file and 
 # prints words of specified lengths (e.g., three, four, 
 # five, etc.) found within the file.
-# Coder:
-# Date:
+# Coder: Shifaa
+# Date: 01/03/2026
 
 print("--- Extracting Words from Text File ---\n")
+
+with open("data.txt", "r") as f:
+    text = f.read()
+
+length = int(input("Enter length of words: "))
+
+words_set = set()
+
+for word in text.split():
+    if len(word) == length:
+        words_set.add(word.lower())
+
+sorted_words = sorted(words_set)
+
+print(f"Words with length {length} are:")
+print(sorted_words)
+
 
